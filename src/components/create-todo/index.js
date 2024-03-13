05f46fcf7094472e3bd241f1c4cfaef32f312853
Nadia-kaf <nadiabokordedzi@gmail.com>
2024-03-13 16:30:16 +0000

    fetching api

--- src/components/create-todo/index.js
+++ src/components/create-todo/index.js
@@ -1,15 +1,25 @@
 import styles from "./index.module.css";
-import {useLocalStorage} from "usehooks-ts";
 import {useState} from "react";
 
 function CreateTodo() {
-     const [todos, setTodos] = useLocalStorage
-    ("TODO_KEY",[]);
+    
     const [todo, setTodo] =useState("");
 
-    const saveTodos =() =>{
-       //Save all todos 
-        setTodos([...todos,todo]);
+    const saveTodos =async () =>{
+       //Post todo to the todo-api
+        const response = await fetch('http://localhost:4000/todos',{
+            method: 'POST',
+            body: JSON.stringify({
+                title: todo
+            }), 
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const data = await response.json();
+        console.log(data);
+
         //wipe the input box
         setTodo("");
     }
@@ -21,7 +31,7 @@ function CreateTodo() {
             <input
             value={todo}
             onKeyDown={event=> event.key == "Enter" && 
-             saveTodos([...todos,todo])}
+             saveTodos()}
              onChange= {event => setTodo(event.target.value)}
              className={styles.createTodoinput1} 
               placeholder ="Start typing..."/>
